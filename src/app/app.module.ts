0cfdeac1bd47b7b3f392e908301998f8ab51f18f
Ann-Annice <annannice98@gmail.com>
2022-10-14 15:54:06 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,30 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+const appRoutes:Routes=[
+  {
+    path:"signup",component:SignupComponent,
+  },
+  {
+    path:"signin",component:SigninComponent,
+  },
+  {
+    path:"dashboard",component:DashboardComponent
+  }
 
+]
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
